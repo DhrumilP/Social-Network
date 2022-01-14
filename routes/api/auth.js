e9d09e67b4b46ceea62a1bd7adf52a76c742b9b1
DhrumilP <dhrumil172@gmail.com>
2022-01-13 20:36:49 -0500

    Get details about authenticated users

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
 
+const User = require('../../models/user');
 
 // @route   GET api/aut
 // @desc    Test route
 // @access  Public
-router.get('/', (req, res) => {
-    
-    res.send('Auth route');
-})
+router.get('/', auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user);
+  } catch (err) {
+    console.log(err.message);
+    res.status();
+  }
+});
 
 module.exports = router;
