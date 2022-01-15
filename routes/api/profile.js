2b7d825013be6055e1e01ed81eea48b073cc8cfb
DhrumilP <dhrumil172@gmail.com>
2022-01-15 17:39:44 -0500

    Get all repos for github username

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const config = require('config');
 const auth = require('../../middleware/auth');
 const Profile = require('../../models/profile');
 const User = require('../../models/user');
 const { check, validationResult } = require('express-validator');
+const axios = require('axios');
 
 // @route   GET api/profile/me
 // @desc    Get current users profile
@@ -289,6 +291,30 @@ router.delete('/education/:edu_id', auth, async (req, res) => {
     await profile.save();
 
     res.json(profile);
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+// @route   GET api/profile/github/:username
+// @desc    Get user repos from GitHub
+// @access  Prublic
+router.get('/github/:username', async (req, res) => {
+  try {
+    const uri = encodeURI(
+      `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc`
+    );
+
+    const headers = {
+      'user-agent': 'node.js',
+      Authorization: `token ${config.get('githubSecret')}`,
+    };
+
+    console.log(headers);
+
+    const gitHubResponse = await axios.get(uri, { headers });
+    return res.json(gitHubResponse.data);
   } catch (err) {
     console.log(err.message);
     res.status(500).send('Server Error');
