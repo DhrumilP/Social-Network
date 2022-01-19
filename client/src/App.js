11a459d3205ddec96f6a739098e203b73a1f919b
DhrumilP <dhrumil172@gmail.com>
2022-01-18 21:30:18 -0500

    Redux boilerplate setup

--- client/src/App.js
+++ client/src/App.js
@@ -5,19 +5,24 @@ import Landing from './components/Landing';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
+//redux
+import { Provider } from 'react-redux';
+import store from './store';
 
 function App() {
   return (
-    <Router>
-      <Fragment>
-        <Navbar />
-        <Route exact path='/' component={Landing} />
-        <section className='container'>
-          <Route exact path='/register' component={Register} />
-          <Route exact path='/login' component={Login} />
-        </section>
-      </Fragment>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <Navbar />
+          <Route exact path='/' component={Landing} />
+          <section className='container'>
+            <Route exact path='/register' component={Register} />
+            <Route exact path='/login' component={Login} />
+          </section>
+        </Fragment>
+      </Router>
+    </Provider>
   );
 }
 
